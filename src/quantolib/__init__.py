e876c5f5cd58b1d714553131a8dc870299d21f11
Jacopo Michelacci <michelacci2005@gmail.com>
2026-03-21 19:43:09 -0500

    updated gitignore

--- src/quantolib/__init__.py
+++ src/quantolib/__init__.py
@@ -1,1 +1,14 @@
+# FROM PLOT
 from .plot import basic_plot
+from .plot import plot_generic_over_time
+
+
+
+# FROM GENERATORS
+from .generators import equity_generator_gbm
+
+
+
+
+# FROM UTILS
+from ._utils import OCContainer1D
